0545632db32a0ad0a1f49fe63295d66d7271fa6b
Sarah Shatto <Sarahshatto@gmail.com>
2021-01-21 16:53:56 -0800

    testing buttons, react router DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import 'semantic-ui-css/semantic.min.css'
+import {BrowserRouter as Router,Route,} from "react-router-dom";
+import  ButtonExampleBasic from './components/button.js'
 
 function App() {
   return (
     <div className="App">
       <h1> Hello, World!</h1>
+      <ButtonExampleBasic/> 
     </div>
   );
 }
