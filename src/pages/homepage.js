f8cf4e0c1e5698e58134a8b20b78909847b661bf
Davee Sok <daveesok@gmail.com>
2021-01-22 12:27:06 -0800

    working on myfeedview

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,37 @@
+import React, { useState } from 'react';
+import { If, Then, Else, When, Unless, Switch, Case, Default } from 'react-if';
+
+
 import HeaderComponent from '../components/header.js';
-import HomeGrid from '../components/grid/homegrid.js'
+import SideBarComponent from '../components/sidebar.js';
+import HomeView from '../components/homeView/homegrid.js'
+import MyFeedView from '../components/myFeedView/myfeedgrid.js'
+
 
 function HomePage() {
+
+  // This state is just to toggle between homeview/myFeedView
+  // This is HARDCODED for DEVELOPMENT PURPOSES
+  const [view, setView] = useState('home');
+
+  function changeToHomeView() {
+    
+  }
 
   return (
     <>
         <HeaderComponent />
-        <HomeGrid/>
+        <SideBarComponent />
+
+        <If condition={view==='feed'}>
+          <Then>
+            <HomeView/>
+          </Then>
+          <Else>
+            <MyFeedView />
+          </Else>
+        </If>
+ 
     </>
   )
 }
