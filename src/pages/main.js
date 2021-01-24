e452080471ca5017485b2585239960c876ad6be6
Davee Sok <daveesok@gmail.com>
2021-01-24 13:18:07 -0800

    cleaned up logic in main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,43 +1,41 @@
 import React, { useState } from 'react';
 import { If, Then, Else, When, Unless, Switch, Case, Default } from 'react-if';
 
-
-import HeaderComponent from '../components/header.js';
-import SideBarComponent from '../components/sidebar.js';
+import Header from '../components/header.js';
+import SideBar from '../components/sidebar.js';
 import HomeView from '../components/homeView/homegrid.js'
 import MyFeedView from '../components/myFeedView/myfeedgrid.js'
 
 
-function HomePage() {
+function Main() {
 
   const [view, setView] = useState('home');
-  const [welcome, setWelcome] = useState('welcome')
+
   function changeView(newView) {
     setView(newView);
   }
 
-  function changeWelcome(newView){
-    setWelcome(newView)
-  }
-  
   return (
     <>
-        <HeaderComponent />
-        <SideBarComponent changeView={changeView} changeWelcome={changeWelcome}/>
+        <Header />
+        <SideBar changeView={changeView}/>
 
-        <If condition={view==='home'}>
-          <Then>
+        <Switch>
+          <Case condition={view==='home'}>
             <HomeView />
-          </Then>
-          {/* <If condition={welcome='welcome'}> */}
-          <Else>
+          </Case>
+
+          <Case condition={view==='myFeed'}>
             <MyFeedView />
-          </Else>
-        </If>
- 
+          </Case>
+
+          <Case condition={view==='welcome'}>
+            <MyFeedView />
+          </Case>
+        </Switch>
+
     </>
   )
 }
 
-
-export default HomePage;
+export default Main;
