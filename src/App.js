d21ea058de24aefa45c1e675cc6737ca3a36cd1d
Davee Sok <daveesok@gmail.com>
2021-01-25 19:51:56 -0800

    can signup user and login

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import store from './store';
 import Auth from './context/auth/auth.js';
 import LoginContext from './context/auth/context.js';
 import Main from './pages/main.js';
-import Signup from './pages/signup/signup.js';
+
 
 import Welcome from './pages/welcome.js';
 
@@ -18,18 +18,12 @@ function App() {
             <Switch>
 
               <Route exact path='/'>
-                <Auth capability="read">
-                  <h1>Will this show</h1>
-                </Auth>
+
                 <Main/>
               </Route>
 
               <Route exact path='/welcome'>
                 <Welcome />
-              </Route>
-
-              <Route exact path='/signup'>
-                <Signup />
               </Route>
 
             </Switch>
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,11 +1,14 @@
+import Signup from '../pages/signup/signup.js';
+import Login from '../context/auth/login.js';
+
 
 function WelcomePage() {
-
-
 
   return (
     <>
       <h1>Welcome Page</h1>
+      <Signup />
+      <Login />
 
     </>
   )
