94be03025dac0837acfa7afe9d2ef9055724fada
jonathon lee <jonnyleealas@gmail.com>
2021-01-26 21:00:20 -0600

    Update CommentGroup.js

--- src/components/list/semanticComponents/CommentGroup.js
+++ src/components/list/semanticComponents/CommentGroup.js
@@ -2,32 +2,32 @@ import React from 'react'
 import { Button, Comment, Form, Header } from 'semantic-ui-react'
 import ListingComment from './Comment.js'
 
-const CommentGroup = (props) => (
+function CommentGroup(props) {
 
   let comments = props.listing.comments;
   return (
-  <Comment.Group size='mini'>
-    <Header as='h3' dividing>
-      Comments
+    <Comment.Group size='mini'>
+      <Header as='h3' dividing>
+        Comments
     </Header>
-    {
-      comments.map(comment => {
-        return (
-        <ListingComment comment={comment} />
-        )
-      })
-    }
+      {
+        comments.map(comment => {
+          return (
+            <ListingComment comment={comment} />
+          )
+        })
+      }
 
-  <br></br>
-    <Form.Group size ='mini'>
-    <Form reply>
-    <Form.Input placeholder='Add a reply...' />
-      <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-    </Form>
-    </Form.Group>
-  
-  </Comment.Group>
-)
-)
+      <br></br>
+      <Form.Group size='mini'>
+        <Form reply>
+          <Form.Input placeholder='Add a reply...' />
+          <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+        </Form>
+      </Form.Group>
+
+    </Comment.Group>
+  )
+}
 
 export default CommentGroup;              
