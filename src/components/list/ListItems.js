318cdbc4e411243110e983fbc5ae28ba11c7c516
Davee Sok <daveesok@gmail.com>
2021-01-27 23:27:26 -0800

    cleared comments and about to add ryans image loader update

--- src/components/list/ListItems.js
+++ src/components/list/ListItems.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import {Grid, Pagination} from 'semantic-ui-react'
 import ListCard from './listcard.js'
 function ListItems (){
-  
+
   let listings = useSelector(state => state.listings)
   let itemsPerPage = 5;
 // *************** Pagination Logic ********************//
@@ -67,53 +67,3 @@ export default ListItems
 
 
 
-// // import React, {useState, useEffect, useContext} from 'react';
-// import { useSelector } from 'react-redux';
-
-// import {Grid, Pagination} from 'semantic-ui-react'
-// import ListCard from './listcard.js'
-
-
-// function ListItems (){
-
-//   let listings = useSelector(state => state.listings)
-
-//   console.log('listings:', listings);
-
-//   const PaginationExampleShorthand = () => (
-//     <Pagination
-//       defaultActivePage={1}
-//       firstItem={null}
-//       lastItem={null}
-//       pointing
-//       secondary
-//       totalPages={3}
-//     />
-//   )
-
-//   return(
-//     <Grid>
-//       <Grid.Row columns={1}>
-//         <Grid.Column textAlign="center" width="12" style={{
-//           margin : "auto"
-//           }}verticalAlign>
-
-//           {
-//             listings.map(item => {
-//               return (
-//                 <ListCard item={item} />
-//               )
-//             })
-//           }
-  
-//         </Grid.Column>
-//       </Grid.Row>
-     
-//       {/* TODO: THIS SHOULD PROBALBY GO IN A FOOTER? OR IN A DIV? PROBABLY NOT IN THE GRID, RESEARCH */}
-//       <PaginationExampleShorthand />
-//     </Grid>
-  
-//   );
-// }
-
-// export default ListItems
