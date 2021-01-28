8e97f47e9eea32b32f5cfe4085241e093a34d31b
RYAN GEDDES <rgeddes09@gmail.com>
2021-01-28 11:09:00 -0800

    polished upload modal

--- src/components/uploadPost/exampleModal.js
+++ src/components/uploadPost/exampleModal.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import { Button, Icon, Image, Modal, Grid, Row, Column } from 'semantic-ui-react'
+// import React from 'react'
+// import { Button, Icon, Image, Modal, Grid, Row, Column } from 'semantic-ui-react'
 
-const UploadModal = (props) => {
-  const [open, setOpen] = React.useState(false)
+// const UploadModal = (props) => {
+//   const [open, setOpen] = React.useState(false)
 
-  return (
+//   return (
  
-    <Modal
-      open={open}
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      trigger={<Button>Create New Post</Button>}
-      >
+//     <Modal
+//       open={open}
+//       onClose={() => setOpen(false)}
+//       onOpen={() => setOpen(true)}
+//       trigger={<Button>Create New Post</Button>}
+//       >
 
     
-      <Modal.Content image scrolling>
-        <Modal.Description >
-          <h1> Upload here </h1> 
-          <p>upload your images</p>
+//       <Modal.Content image scrolling>
+//         <Modal.Description >
+//           <h1> Upload here </h1> 
+//           <p>upload your images</p>
 
-        </Modal.Description>
-      </Modal.Content>
+//         </Modal.Description>
+//       </Modal.Content>
 
-      <Modal.Actions>
-        <Button onClick={() => setOpen(false)} primary>
-          UPLOAD <Icon name='chevron right' />
-        </Button>
-      </Modal.Actions>
+//       <Modal.Actions>
+//         <Button onClick={() => setOpen(false)} primary>
+//           UPLOAD <Icon name='chevron right' />
+//         </Button>
+//       </Modal.Actions>
 
-    </Modal>
+//     </Modal>
    
-  )
-}
+//   )
+// }
 
-export default UploadModal;
+// export default UploadModal;
